10108573811d41badd7d91e68533647051dfa6ac
Ricardo Salazar <ricardotesesi@gmail.com>
2020-11-19 23:35:35 -0600

    version 3

---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../servicios/heroes.service';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html'
+})
+export class HeroeComponent {
+
+  heroe:any={};
+
+  constructor(private activatedRouter: ActivatedRoute,
+              private _heroesService: HeroesService
+    ) {
+
+    this.activatedRouter.params.subscribe( params => {
+      this.heroe=this._heroesService.getHeroe( params['id']);
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  buscarHeroe( termino:string){
+    console.log(termino);
+  }
+
+}
